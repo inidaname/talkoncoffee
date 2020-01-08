097312816db670c625d1db0c1346187d1a03a0af
Hassan Sani <saniyhassan@gmail.com>
2020-01-08 11:43:23 +0100

    Started sculyion with my podcast page

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  links$: Observable<any> = this.scully.available$;
+
+  constructor(
+    private scully: ScullyRoutesService
+  ) { }
+
+  ngOnInit() {
+    this.links$.subscribe(e => console.log(e))
+  }
+
+}
