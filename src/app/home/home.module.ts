78214ed71f6b0df499bb3bb29a934cb6d9eca6dc
Hassan Sani <saniyhassan@gmail.com>
2020-01-18 10:22:59 +0100

    worked on header

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
+
 import { HomeComponent } from './home.component';
 
 
@@ -9,7 +11,8 @@ import { HomeComponent } from './home.component';
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
