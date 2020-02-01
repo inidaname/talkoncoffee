59ada130f5765a31c07585f4944e104610ae0f51
Hassan Sani <saniyhassan@gmail.com>
2020-02-02 00:40:25 +0100

    heading for production

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,15 +3,14 @@ import {NgModule} from '@angular/core';
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './blog.component';
 import { SharedModule } from '../shared/shared.module';
-import {ComponentsModule} from '@scullyio/ng-lib';
+// import {ComponentsModule} from '@scullyio/ng-lib';
 
 
 @NgModule({
   declarations: [BlogComponent],
   imports: [
     BlogRoutingModule,
-    SharedModule,
-    ComponentsModule
+    SharedModule
   ],
 })
 export class BlogModule {}
