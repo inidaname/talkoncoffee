88ddb8395cf26f75dd9e914cc49d3f309a41b5e8
Hassan Sani <saniyhassan@gmail.com>
2020-02-01 15:37:37 +0100

    created a pipe

---
+++ src/app/shared/pipes/url-shorter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'urlShorter'
+})
+export class UrlShorterPipe implements PipeTransform {
+
+  transform(value: string, ...args: any[]): string {
+    let urlRegex = /(https?:\/\/[^\s]+)/g;
+    return value.replace(urlRegex, (url) => {
+        return '<a href="' + url + '" target="_blank">' + url + '</a>';
+    });
+  }
+
+}
