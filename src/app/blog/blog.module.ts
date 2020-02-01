b727cb13ac64ff12cbaa185c9f0f36ea283fcfd9
Hassan Sani <saniyhassan@gmail.com>
2020-02-02 00:41:00 +0100

    remove scully content

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './blog.component';
 import { SharedModule } from '../shared/shared.module';
-// import {ComponentsModule} from '@scullyio/ng-lib';
+// import {ComponentsModule} from '@scullyio/ng-lib'; // remove
 
 
 @NgModule({
